06d89fe6b65c5456cb91657673e5fc40c6e82c00
seyed <this.is.seyed.parsa@gmail.com>
2025-12-16 01:32:14 +0330

    Add commands To CLI

--- ui/cli.py
+++ ui/cli.py
@@ -1,23 +1,72 @@
 import os
+from colorama import Fore
+
 class CLI:
-    def __init__(self, result):
-        self.result = result
+    def __init__(self, ParserFunction):
+        self.parser = ParserFunction
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Welcom to OutlineView Command Line Interface!")
+
+        print("Enter help for show help.")
         while True:
-            level = input("Enter number of Level for show ('all' for show all levels and body texts, 'end' for end):")
-            if level != "all" and level != "end":
+            command = input("OVCLI: ")
+            command = command.split()
+            if command[0] == "help":
+                self.ShowHelp()
+            
+            if command[0] == "add-file":
                 try:
-                    int(level)
+                    command[1]
                 except:
-                    from colorama import Fore
-                    print(Fore.RED, f"'{level}' is not a number. please onle enter number of level.", Fore.RESET)
+                    print(Fore.RED, f"add-file has one parametr: add-file <path>. please trye agane.", Fore.RESET)
                     continue
-            if level == "end":
-                os.system('cls' if os.name == 'nt' else 'clear')
-                print("Good By.")
+                else:    
+                    self.AddFile(command[1])
+
+            if command[0] == "show-to-level":
+                try:
+                    command[1]
+                except:
+                    print(Fore.RED, f"show-to-level has one parametr: show-to-level <level>. please trye agane", Fore.RESET)    
+                else:
+                    try:
+                        self.result
+                    except:
+                        print(Fore.RED, "Not Added A File. Use add-file", Fore.RESET)
+                        continue
+                    else:
+                        if command[1] != "all":
+                            try:
+                                int(command[1])
+                            except:
+                                print(Fore.RED, f"'{command[1]}' is not a number. please onle enter number of level or 'all' for all levels.", Fore.RESET)
+                                continue
+                            else:
+                                self.ShowLevels(command[1])
+                        elif command[1] == "all":
+                            self.ShowLevels(command[1])
+
+            if command[0] == "exit":
+                print("Good By!")
                 break
-            os.system('cls' if os.name == 'nt' else 'clear')
-            self.ShowLevels(level)
     
+    def ShowHelp(self):
+        print("""
+This is OutlineView CLI.
+help                                    show help.
+exit                                    exit from cli.
+add-file <word-file-path>               add a word file with path.
+show-to-level <level:.\example>         show file to <level> level with tree view.
+        """)
+
+    def AddFile(self, path):
+        if os.path.exists(path):
+            self.FilePath = path
+            self.result = self.parser(path)
+        else:
+            print(Fore.RED, f"'{path}' Not Found or it is wrong! please trye agane.", Fore.RESET)
+
+
     def FilterByLevel(self, result, level, current_level=1):
         fresult = []
         for node in result:
@@ -39,13 +88,18 @@ class CLI:
         return fresult
 
     def ShowLevels(self, level):
+        os.system('cls' if os.name == 'nt' else 'clear')
         if level == "all":
             self.ShowInTerminal(self.result)
         else:
             fresult = self.FilterByLevel(self.result, int(level))
             self.ShowInTerminal(fresult)
+        print("\n")
 
     def ShowInTerminal(self, result, first_level=0):
         for item in result:
-            print(first_level*" "+item["data"].text)
-            self.ShowInTerminal(item["childrens"], first_level+1)
+            if item["data"].style.name != "Normal":
+                print(first_level*" "+"~ "+item["data"].text)
+                self.ShowInTerminal(item["childrens"], first_level+1)
+            else:
+                print(first_level*" "+"- "+item["data"].text)
